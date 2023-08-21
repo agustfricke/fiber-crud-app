856491cbab536bbdf4a1f719bc7de00f1c5dd4cc
Agust <agustfricke@protonmail.com>
2023-08-21 19:44:23 -0300

    put router in main.go

--- cmd/main.go
+++ cmd/main.go
@@ -3,6 +3,7 @@ package main
 import (
     "github.com/gofiber/fiber/v2"
     "github.com/gofiber/template/html/v2"
+	"github.com/agustfricke/fiber-crud-app/handlers"
     "github.com/agustfricke/fiber-crud-app/database"
 )
 
@@ -16,7 +17,15 @@ func main() {
         ViewsLayout: "layouts/main", 
 	})
 
-    setupRoutes(app)
+	app.Get("/", handlers.ListTasks)
+    app.Get("/add", handlers.NewTaskView) 
+    app.Post("/add", handlers.CreateTask)
+    app.Get("/success", handlers.ConfirmationView)
+    app.Get("/task/:id", handlers.ShowTask)
+	app.Get("/task/:id/edit", handlers.EditTask)
+	app.Patch("/task/:id", handlers.UpdateTask)
+    app.Delete("/task/:id", handlers.DeleteTask)
+    app.Use(handlers.NotFound)
 
     app.Static("/", "./public")
 
